84c2a0951d04b5d1f624a8486b237e697f9d4559
aitzeng <aitzeng@gmail.com>
2023-04-26 11:39:37 -0700

    Added route

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const router = require('./routes.js');
+
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
+app.use('/glossary', router)
+
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
