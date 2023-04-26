effc387fc2c73076b993fa2670d2e7e319fd4c00
aitzeng <aitzeng@gmail.com>
2023-04-26 13:21:10 -0700

    Added save feature

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/glossary');
+
+const glossarySchema = mongoose.Schema({
+  word: String,
+  definition: String
+})
+
+const Repo = mongoose.model('Repo', glossarySchema)
+
+let save = (wordObject) => {
+  let objInstance = new Repo(wordObject)
+
+  return objInstance.save();
+}
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+module.exports.save = save;
