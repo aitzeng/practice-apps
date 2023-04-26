22e582acf3868e31d2e9ad89f4cef97c7cf1e29c
aitzeng <aitzeng@gmail.com>
2023-04-26 15:21:26 -0700

    Updated db to organize alphabetically

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -14,9 +14,14 @@ let save = (wordObject) => {
   return objInstance.save();
 }
 
+let grabDictionary = () => {
+  return Repo.find().sort({word: 1}).exec()
+}
+
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
 
 module.exports.save = save;
+module.exports.grabDictionary = grabDictionary;
