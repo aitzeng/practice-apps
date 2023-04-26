e3c3f4913d74cc3dced877bbd1e0c0adc031702b
aitzeng <aitzeng@gmail.com>
2023-04-26 11:39:15 -0700

    Updated

--- 1-glossary/client/src/components/AddWord.jsx
+++ 1-glossary/client/src/components/AddWord.jsx
@@ -1,7 +1,26 @@
-const AddWord = () => {
+import React from 'react';
+
+const { useState, useEffect } = React;
+
+const AddWord = ({submitAdd}) => {
+
+  const [newWord, setNewWord] = useState('');
+  const [newDef, setNewDef] = useState('');
+
+  const submitWord = (e) => {
+    e.preventDefault();
+    submitAdd(newWord, newDef)
+    setNewWord('');
+    setNewDef('');
+  }
 
   return (
 
+    <form>
+      <input type="text" onChange={setNewWord} placeholder="Word"/>
+      <input type="text" onChange={setNewDef} placeholder="Definition"/>
+      <input type="submit" value="Add Word" onSubmit={}/>
+    </form>
 
   )
 }
