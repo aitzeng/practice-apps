aae4fa3e40a7dd2fe16ed92bfb49e17606527177
aitzeng <aitzeng@gmail.com>
2023-04-26 17:38:15 -0700

    Updated delete express

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -18,10 +18,22 @@ let grabDictionary = () => {
   return Repo.find().sort({word: 1}).exec()
 }
 
+let updateDictionary = (updateObject) => {
+  let filter = {_id: updateObject._id}
+  let update = {word: updateObject.word, definition: updateObject.definition}
+  return Repo.findOneAndUpdate(filter, update);
+
+}
+
+let deleteDictionary = (deleteID) => {
+  let target = {_id: deleteID}
+  return Repo.deleteOne(target)
+
+}
+
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
 
-module.exports.save = save;
-module.exports.grabDictionary = grabDictionary;
+module.exports = { save, grabDictionary, updateDictionary, deleteDictionary }
