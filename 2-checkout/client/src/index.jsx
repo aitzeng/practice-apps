45f87313249c837ef0306509ed2d2d299ab475b7
aitzeng <aitzeng@gmail.com>
2023-04-28 00:52:38 -0700

    Added App component to render

--- 2-checkout/client/src/index.jsx
+++ 2-checkout/client/src/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import App from './components/App.jsx';
+
+// const { useState, useEffect } = React;
+
+// const [page, setPage ] = useState('')
 
 render(
+
   <div>
     <p>Hello, World!</p>
     <p>
       <code>Page Cookie: {JSON.stringify(document.cookie, undefined, "\t")}</code>
     </p>
+    <App />
   </div>,
   document.getElementById("root")
 );
+
