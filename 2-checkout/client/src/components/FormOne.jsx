a023fcb55a8772da21079bc62476df95270c501e
aitzeng <aitzeng@gmail.com>
2023-04-28 11:05:43 -0700

    Updated to submit name, email, password

--- 2-checkout/client/src/components/FormOne.jsx
+++ 2-checkout/client/src/components/FormOne.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const { useState, useEffect } = React;
 
-const FormOne = ({addInfo}) => {
+const FormOne = ({setCurrentForm, userInfo, setUserInfo}) => {
 
   const [typeName, setTypeName] = useState('');
   const [elecMail, setElecMail] = useState('');
@@ -22,16 +22,19 @@ const FormOne = ({addInfo}) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    addInfo({name: typeName, email: elecMail, password: pass})
+    setUserInfo({...userInfo, name: typeName, email: elecMail, password: pass})
     setTypeName('');
     setElecMail('');
     setPassword('');
+    setCurrentForm(2);
 
   }
 
+
   return (
 
-    <form>
+    <form onSubmit={submitHandler}>
+      <input type="submit" value="Next"/>
       <input type="text" value={typeName} onChange={nameChanger} placeholder="name"/>
       <input type="text" value={elecMail} onChange={emailChanger} placeholder="email"/>
       <input type="text" value={pass} onChange={passChanger} placeholder="passowrd"/>
