94e0dbaaae52df2bb8d0e88ddb324a30f080291b
aitzeng <aitzeng@gmail.com>
2023-04-28 11:05:26 -0700

    Updated component to each render own info

--- 2-checkout/client/src/components/FormList.jsx
+++ 2-checkout/client/src/components/FormList.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import FormOne from './FormOne.jsx';
+import FormTwo from './FormTwo.jsx';
 
 const { useState, useEffect } = React;
 
-const FormList = ({currentForm}) => {
+const FormList = ({currentForm, setCurrentForm}) => {
 
   const [userInfo, setUserInfo] = useState({});
 
-  const addInfo = (info) => {
-    setUserInfo({...userInfo, ...info})
-  }
+
+  // const formSubmitter = (e, dataToAdd) => {
+  //   e.preventDefault();
+  //   if (currentForm === 4) { //Axios POST Request
+  //     setCurrentForm(0)
+  //   } else if (currentForm === 0) {
+  //     setCurrentForm(currentForm + 1)
+  //   } else {
+  //     // setUserInfo({...userInfo, ...dataToAdd})
+  //     setCurrentForm(currentForm + 1)
+  //   }
+  // }
 
   if (currentForm === 0) {
     return (
@@ -20,8 +30,21 @@ const FormList = ({currentForm}) => {
   if (currentForm === 1) {
     return (
       <div>
-        <FormOne addInfo={addInfo}/>
+        <FormOne setCurrentForm={setCurrentForm} userInfo={userInfo} setUserInfo={setUserInfo}/>
       </div>
+    )
+  }
+
+  if (currentForm === 2) {
+
+    console.log(userInfo);
+
+    return (
+
+      <div>
+        <FormTwo />
+      </div>
+
     )
   }
 }
