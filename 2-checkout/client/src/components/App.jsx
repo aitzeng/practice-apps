3b904f84e0c3233a8152fcec5d460299b76cf6cb
aitzeng <aitzeng@gmail.com>
2023-04-28 11:04:54 -0700

    Updated App and moved submit button to component

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -6,33 +6,31 @@ const { useState, useEffect } = React;
 const App = () => {
 
   const [currentForm, setCurrentForm] = useState(0)
+  // const [check, setCheck] = useState(false)
 
-  const formSubmitter = (e) => {
+
+  const beginCheckOut = (e) => {
     e.preventDefault();
-    if (currentForm === 4) {
-      setCurrentForm(0)
-    } else {
-      setCurrentForm(currentForm + 1)
-    }
+    // setCheck(!check);
+    setCurrentForm(currentForm + 1)
   }
 
-  let submitValue;
-  if (currentForm === 0) {
-    submitValue = "Checkout";
-  } else if (currentForm === 4) {
-    submitValue = "Purchase";
-  } else {
-    submitValue = "Next";
-  }
+  // let submitValue;
+  // if (currentForm === 0) {
+  //   submitValue = 'Checkout'
+  //   setCheck(false)
+  // } else {
+  //   setCheck(true)
+  // }
 
   return (
 
 
     <div>
-      <form onSubmit={formSubmitter}>
-        <input type="submit" value={submitValue}/>
+      <form onSubmit={beginCheckOut}>
+        {currentForm === 0 ? <input type="submit" value="Checkout" disabled={false}/> : <input type="submit" value="Checkout" disabled={true}/>}
       </form>
-      <FormList currentForm = {currentForm}/>
+      <FormList currentForm = {currentForm} setCurrentForm={setCurrentForm}/>
     </div>
 
   )
