017ccf66d337848f2503f2e938709f4caa8ac163
aitzeng <aitzeng@gmail.com>
2023-04-29 16:05:19 -0700

    Creates table and connects to database

--- 2-checkout/server/db.js
+++ 2-checkout/server/db.js
@@ -16,11 +16,7 @@ db.connectAsync() // Connects to the mySQL server with the credentials
   .then(() =>
     // Expand this table definition as needed:
     db.queryAsync(
-      "CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, email VARCHAR(25) NOT NULL, password VARCHAR(20) NOT NULL, address1 VARCHAR(25) NOT NULL, address2 VARCHAR(25) NULL, city VARCHAR(20) NOT NULL, state VARCHAR(20) NOT NULL, zipcode VARCHAR(20) NOT NULL, phone VARCHAR(20) NOT NULL);"
-  ))
-  .then(() =>
-  db.queryAsync(
-    "CREATE TABLE IF NOT EXISTS payment (id INT, creditcard INT NOT NULL, cvv VARCHAR(20) NOT NULL, expiration DATE NOT NULL, billzip INT NOT NULL, FOREIGN KEY (id) REFERENCES responses(id));"
+      "CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, session_id VARCHAR(50) NOT NULL, name VARCHAR(20) NOT NULL, email VARCHAR(25) NOT NULL, password VARCHAR(20) NOT NULL, address1 VARCHAR(25) NOT NULL, address2 VARCHAR(25) NULL, city VARCHAR(20) NOT NULL, state VARCHAR(20) NOT NULL, zipcode INT NOT NULL, phone VARCHAR(20) NOT NULL, creditcard VARCHAR(25) NOT NULL, cvv INT NOT NULL, expiration VARCHAR(10) NOT NULL, billzip INT NOT NULL);"
   ))
   .catch((err) => console.log(err));
 
