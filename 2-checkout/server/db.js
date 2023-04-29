41f40a0f4d2a4cc1ecbcc82d0d80d21181cf53ff
aitzeng <aitzeng@gmail.com>
2023-04-28 17:30:38 -0700

    Created tables

--- 2-checkout/server/db.js
+++ 2-checkout/server/db.js
@@ -11,14 +11,17 @@ const connection = mysql.createConnection({
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
 
-db.connectAsync()
+db.connectAsync() // Connects to the mySQL server with the credentials
   .then(() => console.log(`Connected to MySQL as id: ${db.threadId}`))
   .then(() =>
     // Expand this table definition as needed:
     db.queryAsync(
-      "CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)"
-    )
-  )
+      "CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, email VARCHAR(25) NOT NULL, password VARCHAR(20) NOT NULL, address1 VARCHAR(25) NOT NULL, address2 VARCHAR(25) NULL, city VARCHAR(20) NOT NULL, state VARCHAR(20) NOT NULL, zipcode VARCHAR(20) NOT NULL, phone VARCHAR(20) NOT NULL);"
+  ))
+  .then(() =>
+  db.queryAsync(
+    "CREATE TABLE IF NOT EXISTS payment (id INT, creditcard INT NOT NULL, cvv VARCHAR(20) NOT NULL, expiration DATE NOT NULL, billzip INT NOT NULL, FOREIGN KEY (id) REFERENCES responses(id));"
+  ))
   .catch((err) => console.log(err));
 
 module.exports = db;
