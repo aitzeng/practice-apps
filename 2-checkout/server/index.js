50af9aa2d3e69b30618089f9f38f5423787876b8
aitzeng <aitzeng@gmail.com>
2023-04-28 17:30:47 -0700

    Created app.post

--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -16,15 +16,23 @@ app.use(sessionHandler);
 // Logs the time, session_id, method, and url of incoming requests.
 app.use(logger);
 
+// Converts JSON data
+app.use(express.json());
+
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+app.post('/checkout', (req, res) => {
+  res.send('Request Received')
+  console.log('This is req.body:', req.body)
+  console.log('This is req.session_id:', req.session_id)
+})
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
